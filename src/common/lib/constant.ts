b62b780438b9d76dee1faa96395de4d723e61d3b
Koji Miyauchi <koji@supermojo.com>
2024-06-06 11:10:00 -0400

    Add cog to update api key

--- src/common/lib/constant.ts
+++ src/common/lib/constant.ts
@@ -7,4 +7,4 @@ export const BACKEND_API_URL =
   process.env.NEXT_PUBLIC_BACKEND_API_URL || "http://localhost:3000/";
 
 export const SKYFIRE_API_KEY =
-  process.env.NEXT_PUBLIC_SKYFIRE_API_KEY
+  process.env.NEXT_PUBLIC_SKYFIRE_API_KEY || '2474059e-f1e2-49f8-aa20-236e16d2c0c5';
